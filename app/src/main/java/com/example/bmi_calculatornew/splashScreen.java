0edda3497e9159ee8b90e893d5fd158055bc4c1d
it20240738 <it20240738@my.sliit.lk>
2021-12-02 01:20:20 +0530

    Splash screen create

---
+++ app/src/main/java/com/example/bmi_calculatornew/splashScreen.java
@@ -0,0 +1,31 @@
+package com.example.bmi_calculatornew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+//@SuppressLint("CustomSplashScreen")
+public class splashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(splashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
+    }
+}
